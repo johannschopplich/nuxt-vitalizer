8f8f7494efe6e04ac8651109b301903e15e75448
Johann Schopplich <mail@johannschopplich.com>
2024-11-05 11:31:05 +0100

    chore: upgrade dependencies

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,6 +1,6 @@
 import NuxtVitalizer from '../src/module'
 
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
   modules: [NuxtVitalizer],
+  compatibilityDate: '2024-04-03',
 })
